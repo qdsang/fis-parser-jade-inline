3d2431f40b2a3cd1e534f2431ff64dffa8ae2623
fenglong <fenglong@xiaomi.com>
2014-03-06 13:58:41 +0800

    功能完成

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-fis-parser-jade
+fis-parser-jade-inline
 ===============
 
-A parser for fis to compile jade file.
+A parser for fis to compile inline jade file.
+
+内嵌jade模板， 编译后的模板依赖runtime
+
+```
+fis.config.merge({
+    project : {
+        fileType : {
+            text : 'jtpl'
+        }
+    },
+    modules : {
+        parser : {
+            jtpl : 'jade-inline'
+        }
+    },
+    settings : {
+        parser : {
+            'jade-inline': {
+                isInlineRuntime : true
+            }
+        }
+    }
+});
+```
--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-/*
- * fis
- * http://fis.baidu.com/
- */
 
 'use strict';
 
 
-var jade = require('jade');
+var jade = require('jade')
+    , runtimejs = '';
 
 module.exports = function(content, file, conf){
-  return  jade.compile( content, conf )({});
+    conf.compileDebug = false;
+
+    var tmpl = jade.compileClient( content, conf ),
+        fn = '', fnstr = '';
+
+
+    if (conf.isInlineRuntime !== false) {
+        if (runtimejs == '') {
+            try {
+                var runtimefile = __dirname + '/node_modules/jade/runtime.js';
+                runtimejs = require('fs').readFileSync(runtimefile, 'utf8')
+            } catch (ex) {
+                runtimejs = ' ';
+                console.log('jade-inline: jade.runtime.js read file error' + ex);
+            }
+        }
+        fn += runtimejs;
+    }
+
+    // 兼容旧版本 jade
+    fn += 'if (typeof template == \'undefined\'){template = anonymous;} ';
+    fn += 'return template(locals);'+ tmpl.toString();
+    fnstr = 'function(locals){' + fn + '};'
+    return fnstr;
 };
